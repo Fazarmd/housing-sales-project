9c264b66c4bb3636a908176fb1331926f093b247
Fazarmd <mfazarmd@gmail.com>
2023-12-27 16:53:09 +0700

    feat: add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import bgImage from "../assets/bgImg.jpg";
+
+const Hero = () => {
+  return (
+    <div className="w-full h-screen">
+      <img className="top-0 left-0 w-full h-screen object-cover" src={bgImage} alt="/" />
+      <div className="bg-black/30 absolute top-0 left-0 w-full h-screen" />
+      <div className="absolute top-0 w-full h-full flex flex-col justify-center text-white">
+        <div className="md:left-[10%] max-w-[1100px] m-auto absolute p-4">
+          <p className="text-sm mb-5">Creating Neighborhoods, Building Houses</p>
+          <h1 className="font-bold text-5xl md:text-7xl drop-shadow-2xl">Find Your Perfect Home</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Bars2Icon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
@@ -12,9 +12,26 @@ const Navbar = () => {
   ];
 
   const [open, setOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 50;
+      if (isScrolled !== scrolled) {
+        setScrolled(!scrolled);
+      }
+    };
+
+    document.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      // cleanup
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrolled]);
 
   return (
-    <div className="w-screen z-10 bg-teal-950 fixed drop-shadow-lg">
+    <div className={`w-screen z-10 fixed drop-shadow-lg ${scrolled ? "bg-teal-950" : "bg-transparent"}`}>
       <div className="px-4 flex justify-between items-center h-[80px]">
         <div className="flex items-center">
           <h1 className="text-3xl font-bold text-gray-100 p-2">BRAND.</h1>
@@ -23,7 +40,7 @@ const Navbar = () => {
           <Bars2Icon className="cursor-pointer h-6 w-6 text-gray-100" onClick={() => setOpen(!open)} />
         </div>
         <div className="hidden md:flex">
-          <ul className="flex bg-teal-950 text-gray-100 items-center">
+          <ul className={`flex text-gray-100 items-center ${scrolled ? "bg-teal-950" : "bg-transparent"}`}>
             {menus?.map((menu, i) => (
               <Link to={menu?.link} key={i} className="cursor-pointer hover:text-gray-500">
                 <li className="py-2">{menu?.name}</li>
@@ -33,7 +50,7 @@ const Navbar = () => {
         </div>
       </div>
       <div className={`transition-all duration-1000 ease-in-out overflow-hidden ${open ? "opacity-100 max-h-screen" : "opacity-100 max-h-0"}`}>
-        <ul className="flex flex-col bg-teal-950 text-gray-100 w-full md:hidden items-center">
+        <ul className={`flex flex-col text-gray-100 w-full md:hidden items-center ${scrolled ? "bg-teal-950" : "bg-transparent"}`}>
           {menus?.map((menu, i) => (
             <Link to={menu?.link} key={i} className="cursor-pointer hover:text-gray-500 w-full">
               <li className="ml-3 mr-3 border-b-2 border-teal-900">{menu?.name}</li>
