1afe47abcfd9154d717ebda2d4a82f4794245e1c
Fazarmd <mfazarmd@gmail.com>
2023-12-27 10:52:54 +0700

    style: add borders to menu items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     <div className="w-screen z-10 bg-teal-950 fixed drop-shadow-lg">
       <div className="px-4 flex justify-between items-center h-[80px]">
         <div className="flex items-center">
-          <h1 className="text-3xl font-bold text-gray-100 sm:text-4xl p-2">BRAND.</h1>
+          <h1 className="text-3xl font-bold text-gray-100 p-2">BRAND.</h1>
         </div>
         <div className="md:hidden flex justify-end p-2">
           <Bars2Icon className="cursor-pointer h-6 w-6 text-gray-100" onClick={() => setOpen(!open)} />
@@ -32,11 +32,11 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-      <div className={`px-4 py-2 transition-all duration-1000 ease-in-out overflow-hidden ${open ? "opacity-100 max-h-screen" : "opacity-100 max-h-0"}`}>
+      <div className={`transition-all duration-1000 ease-in-out overflow-hidden ${open ? "opacity-100 max-h-screen" : "opacity-100 max-h-0"}`}>
         <ul className="flex flex-col bg-teal-950 text-gray-100 w-full md:hidden items-center">
           {menus?.map((menu, i) => (
-            <Link to={menu?.link} key={i} className="cursor-pointer hover:text-gray-500">
-              <li className="py-2">{menu?.name}</li>
+            <Link to={menu?.link} key={i} className="cursor-pointer hover:text-gray-500 w-full">
+              <li className="ml-3 mr-3 border-b-2 border-teal-900">{menu?.name}</li>
             </Link>
           ))}
         </ul>
